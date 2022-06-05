fe97d1daeb8c891cffc0bbd607dab37ae51e11af
Lucas Emanoel <102261954+lucasetdasilva@users.noreply.github.com>
2022-06-05 14:35:23 -0300

    Add files via upload

---
+++ classe.py
@@ -0,0 +1,23 @@
+class listadesejo:
+    nomecidade = ""
+    estado = ""
+    pontoturistico = ""
+
+    def setnomecidade(self,n):
+        self.nomecidade = n
+
+    def setestado(self2,n2):
+        self2.estado = n2
+
+    def setponto(self3,n3):
+        self3.pontoturistico = n3
+
+    def getnomecidade(self):
+        return self.nomecidade
+
+    def getestado(self2):
+        return self2.estado
+
+    def getponto(self3):
+        return self3.pontoturistico
+    
